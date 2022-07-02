292f8f30d98029abc9fa9a900e75f5a2b569d17c
Tran Quang Thai <tqthai2001@gmail.com>
2022-07-02 23:58:50 +0700

    report & presentation

--- Virus/src/hust/soict/globalict/Controller/MainScreenController.java
+++ Virus/src/hust/soict/globalict/Controller/MainScreenController.java
@@ -14,7 +14,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 
-public class MainScreenController implements Initializable{
+public class MainScreenController implements Initializable {
 	private HIV hiv;
 	private Covid19 covid;
 	private Herpes herpes;
@@ -75,37 +75,37 @@ public class MainScreenController implements Initializable{
 
     @FXML
     void hivBtnPressed(ActionEvent event) {
-//    	jFrame.setVisible(false);
+    	jFrame.setVisible(false);
     	new StructureScreen(hiv, jFrame);
     }
 
     @FXML
     void covid19Btn(ActionEvent event) {
-//    	jFrame.setVisible(false);
+    	jFrame.setVisible(false);
     	new StructureScreen(covid, jFrame);
     }
 
     @FXML
     void herpesBtn(ActionEvent event) {
-//    	jFrame.setVisible(false);
+    	jFrame.setVisible(false);
     	new StructureScreen(herpes, jFrame);
     }
 
     @FXML
     void rotaBtn(ActionEvent event) {
-//    	jFrame.setVisible(false);
+    	jFrame.setVisible(false);
     	new StructureScreen(rota, jFrame);
     }
 
     @FXML
     void astroBtn(ActionEvent event) {
-//    	jFrame.setVisible(false);
+    	jFrame.setVisible(false);
     	new StructureScreen(astro, jFrame);
     }
 
     @FXML
     void adenoBtn(ActionEvent event) {
-//    	jFrame.setVisible(false);
+    	jFrame.setVisible(false);
     	new StructureScreen(adeno, jFrame);
     }
     
